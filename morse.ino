c756138f4bf552ebf16fb5d04f35bd2a0f911610
jade <125107843+j-adia@users.noreply.github.com>
2024-09-27 00:20:48 -0400

    Update morse.ino

--- morse.ino
+++ morse.ino
@@ -1,8 +1,9 @@
 // source file
 #include "morse.h"
 
-const int buzzer = 12;
-const int speed = 2;
+const int buzzer = 12; // buzzer connected to pin 12
+const int speed = 2;   // adjust the speed of the message: 1(slower) - 2.7(faster)
+
 bool on = true;
 
 void setup() {
