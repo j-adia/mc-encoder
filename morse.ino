814d72724df7a4ed2815f1b6366066dede16b2ac
jade <125107843+j-adia@users.noreply.github.com>
2024-09-27 00:15:26 -0400

    Update morse.ino

--- morse.ino
+++ morse.ino
@@ -6,15 +6,13 @@ const int speed = 2;
 bool on = true;
 
 void setup() {
-  // put your setup code here, to run once:
   pinMode(buzzer, OUTPUT);
 }
 
 void loop() {
-  char message[] = "jadia";
+  char message[] = "hello world";
   int message_length = strlen(message);
-
-  // put your main code here, to run repeatedly:
+  
   if (on){
     parseMorseCode(message, message_length);
     on = false;
